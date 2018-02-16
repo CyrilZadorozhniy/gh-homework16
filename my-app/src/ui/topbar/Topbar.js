de4beba0c60c5bbf3d192ce375c659883c82b356
CyrilZadorozhniy <kirildoan14@gmail.com>
2018-02-16 20:28:25 +0200

    homework 16 Adding some elements to the component topbar

--- my-app/src/ui/topbar/Topbar.js
+++ my-app/src/ui/topbar/Topbar.js
@@ -10,6 +10,8 @@ import Avatar from 'material-ui/Avatar';
 import Popover from 'material-ui/Popover';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
+import Badge from 'material-ui/Badge';
+import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 
 const style = {
     iconButton: {
@@ -54,7 +56,21 @@ class Toptbar extends React.Component {
 
                 </div>
                 <div className="right-container">
-                    <div className="avatar-button" style={{ transform: [{ rotate: '180deg'}]}}>
+                    <button className="button-add">Add project</button>
+                    <IconButton style={{marginLeft: 15}}>
+                        <i className="material-icons">mail</i>
+                    </IconButton>
+                    <Badge
+                        style={{padding: 0, paddingRight: 20}}
+                        badgeContent={3}
+                        primary={true}
+                        badgeStyle={{top: 5, right: 18, border: '2px solid #ffffff'}}
+                    >
+                        <IconButton tooltip="Notifications">
+                            <NotificationsIcon />
+                        </IconButton>
+                    </Badge>
+                    <div className="avatar-button">
                         <IconButton style={style.iconButton}  onClick={this.handleClick}>
                             <Avatar src={User} size={50}  />
                         </IconButton>
